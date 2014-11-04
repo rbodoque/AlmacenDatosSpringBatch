665e1e6bc2703e5a788bc3f69bbe9c3089df88a6
rafa <rsbodoque@gmail.com>
2014-11-04 01:39:31 +0100

    casa2

--- AlmacenDatosSpringBatch/src/main/java/es/gfi/batch/readers/dto/InstanciaMetrica.java
+++
@@ -1,8 +0,0 @@
-package es.gfi.batch.readers.dto;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-public class InstanciaMetrica {
-
-}
